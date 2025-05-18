186b93fd16f773358cdf109ef6c10bb002afc19f
mason young <masonyoung1997@gmail.com>
2025-05-18 16:45:56 -0700

    Fix conflicting global and local allPokemon variables

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,14 @@
 const allPokemon = [];
 async function fetchPokemon() {
     try {
-        const allPokemon = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=10000");
+        allPokemon = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=10000");
         console.log(allPokemon);
     } catch (err) {
         console.error("Error fetching Pokemon:", err.message)
     }
 }
 
-function selectRandomItem(arr) {
+function getRandomElement(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
